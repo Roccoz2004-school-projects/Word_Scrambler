b26ba0262ede08f9ac542d05bc2516101f0ee64a
Roccoz2004 <roccoz2004@gmail.com>
2022-05-10 23:41:48 -0700

    Added WordScrambler class

---
+++ src/dev/rocco/WordScrambler.java
@@ -0,0 +1,36 @@
+package dev.rocco;
+
+public class WordScrambler {
+
+    private String[] scrambledWords;
+
+    /**
+     * @param wordArr an array of String objects
+     *                Precondiion: wordArr . length is even
+     */
+
+    public WordScrambler(String[] wordArr) {
+        scrambledWords = mixedWords(wordArr);
+    }
+
+    /**
+     * @param wordl a String of characters
+     * @param word2 a String of characters
+     * @return a String that contains the first half of wordl and the second half of we
+     */
+    private String recombine(String wordl, String word2) {
+
+        return wordl;
+    }
+    /* to he implemented in part */
+
+
+    private String[] mixedWords(String[] words) {
+
+        return words;
+    }
+
+}
+
+
+   /* to be implemented in part */
